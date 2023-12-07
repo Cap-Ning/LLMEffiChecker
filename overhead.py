88b74b8a9028e8b89bf513d352e7ebc8742b16bf
Cap-Ning <131382659+Cap-Ning@users.noreply.github.com>
2023-12-07 10:22:05 +0800

    Update overhead.py

--- overhead.py
+++ overhead.py
@@ -10,52 +10,78 @@ from utils import *
 if not os.path.isdir('adv'):
     os.mkdir('adv')
 
+MODEL_NAME_LIST = [
+    'Helsinki-en-zh',
+    'facebook-wmt19',
+    'T5-small',
+    'allenai-wmt16',
+    'DDDSSS',
+    'unicamp',
+    'flan-t5-small',
+]
+# MODEL_NAME_LIST = [
+#     'LaMini-GPT',
+# ]
 
-def main():
-    final_res = []
-    for data_name in MODEL_NAME_LIST:
-        for attack_type in [0, 1]:
-            device = torch.device('cuda')
-            model, tokenizer, space_token, dataset = load_model_dataset(data_name)
-            print('load model %s successful' % data_name)
-            config = {
-                'num_beams': 1,
-                'num_beam_groups': 1,
-                'max_per': 5,
-                'max_len': 100
-            }
-            if attack_type == 0:
-                attack = CharacterAttack(model, tokenizer, space_token, device, config)
-                attack_name = 'C'
-            elif attack_type == 1:
-                attack = WordAttack(model, tokenizer, space_token, device, config)
-                attack_name = 'W'
+for data_name in MODEL_NAME_LIST:
+    for attack_type in [0, 1, 6, 7, 8, 9]:
+        succ_num=0
+        device = torch.device('cuda')
+        model, tokenizer, space_token, dataset, src_lang, tgt_lang = load_model_dataset(data_name)
+        print('load model %s successful' % data_name)
+        config = {
+            'num_beams': model.config.num_beams,
+            'num_beam_groups': model.config.num_beam_groups,
+            'max_per': 3,
+            'max_len': 100,
+            'src': src_lang,
+            'tgt': tgt_lang
+        }
+        if attack_type == 0:
+            attack = CharacterAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'C'
+        elif attack_type == 1:
+            attack = WordAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'W'
+        elif attack_type == 6:
+            attack = StructureAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'S'
+        elif attack_type == 7:
+            attack = Black_box_CharacterAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'BC'
+        elif attack_type == 8:
+            attack = Black_box_StructureAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'BW'
+        elif attack_type == 9:
+            attack = Black_box_WordAttack(model, tokenizer, space_token, device, config)
+            attack_name = 'BS'        
+        else:
+            raise NotImplementedError
+        results = []
+        for i, src_text in enumerate(dataset):
+            if succ_num>=20:
+                break
+            if i == 0:
+                continue
+            if i >= 100:
+                break
+            src_text = src_text.replace('\n', '')
+            if attack_type in [7,8,9]:
+                is_success, adv_his = attack.run_black_attack([src_text])
             else:
-                raise NotImplementedError
-            results = []
-            for i, src_text in enumerate(dataset):
-                if i == 0:
-                    continue
-                if i >= 10:
-                    break
-                src_text = src_text.replace('\n', '')
                 is_success, adv_his = attack.run_attack([src_text])
-                if is_success:
-                    overhead = np.array([r[-1] for r in adv_his]).reshape([1, -1])
-                    results.append(overhead)
-            try:
-                results = np.concatenate(results)[:, 1:]
-            except:
-                print()
-            mean, std = results.mean(0), results.std(0)
-            final_res.append(np.array([mean, std]).reshape([1, -1]))
-    final_res = np.concatenate(final_res)
-    np.savetxt('sim/overhead.csv', final_res, delimiter=',')
+            if is_success:
+                succ_num=succ_num+1
+                overhead = np.array([r[-1] for r in adv_his]).reshape([1, -1])
+                results.append(overhead)
+        print(np.concatenate(results)[:,1:].mean(0))
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Transformer')
-    parser.add_argument('--data', default=2, type=int, help='experiment subjects')
-    parser.add_argument('--attack', default=1, type=int, help='attack type')
-    args = parser.parse_args()
-    main()
+
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='Transformer')
+#     parser.add_argument('--data', default=2, type=int, help='experiment subjects')
+#     parser.add_argument('--attack', default=1, type=int, help='attack type')
+#     args = parser.parse_args()
+#     main()
