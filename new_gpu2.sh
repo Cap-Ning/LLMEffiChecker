d4fcaa24956b6052ffe2f71dcb29d3ee3a109684
Cap-Ning <131382659+Cap-Ning@users.noreply.github.com>
2023-12-07 10:23:59 +0800

    Update new_gpu2.sh

--- new_gpu2.sh
+++ new_gpu2.sh
@@ -7,4 +7,4 @@
 #CUDA_VISIBLE_DEVICES=2 python measure_latency.py --data=1 --attack=3
 
 
-CUDA_VISIBLE_DEVICES=2 python measure_senstive.py --data=1 --attack=0
+CUDA_VISIBLE_DEVICES=2 python measure_senstive.py --data=1 --attack=1
